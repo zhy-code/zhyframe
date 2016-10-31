6107b66507034ffebbc14fcc04954cd0ba6a67d9
zhy <564197281@qq.com>
2016-10-31 16:40:49 +0800

    duoyu

--- 城邦网/招聘登录/yd_zp/js/clogin.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$("body").on('mousemove','.company-menu ul li',function(){
-		$(this).addClass('active').siblings().removeClass('active');
-		$(this).parents('.company-menu').siblings('.company-logo-box').find('ul').eq($(this).index()).show().siblings('ul').hide();
-	});
-});
